de6f64993c1eb699648d6430194cc7a3ec54bda2
SahTitus <sahtitus58@gmail.com>
2022-08-18 01:32:31 +1200

    commit

--- index.js
+++ index.js
@@ -22,17 +22,15 @@ connectDB();
 
 app.use(cors(corsOptions));
 app.use(credentials);
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
-    "Origin, X-Requested-with, Content_Type,Accept,Authorization",
-    "https://hook-murex.vercel.app"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
+app.use((req,res, next)=>{
+ 
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+res.setHeader('Access-Control-Allow-Origin',"*");
+res.setHeader('Access-Control-Allow-Headers',"*");
+res.header('Access-Control-Allow-Credentials', true);
+next();
 });
 
 // app.set('trust proxy', 1)
