da4bd219950cc5a099b6cd9f74aa120494bcd4e1
SahTitus <sahtitus58@gmail.com>
2022-08-18 01:39:09 +1200

    co

--- index.js
+++ index.js
@@ -22,16 +22,6 @@ connectDB();
 
 app.use(cors(corsOptions));
 app.use(credentials);
-app.use((req,res, next)=>{
- 
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-res.setHeader('Access-Control-Allow-Origin',"*");
-res.setHeader('Access-Control-Allow-Headers',"*");
-res.header('Access-Control-Allow-Credentials', true);
-next();
-});
 
 // app.set('trust proxy', 1)
 // app.use(
